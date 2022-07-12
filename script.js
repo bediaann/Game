f03b1a8d5686fd78d78fe4c441dc01069b2bb737
tatevyan <tatev.h2@tumo.org>
2022-07-12 18:34:05 +0400

    changes for js & css

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnStart = document.querySelector(".control");
+const modalWindow = document.querySelector(".modal");
+const newGameBtn = document.querySelector(".modal .input_btn");
+const input = document.querySelector(".modal .inputcell");
+const gameArea = document.querySelector(".game");
+const title = document.querySelector(".area.game .title");
+const controlBtn = document.querySelector(".contro");
+const restartBtn = document.querySelector('.btn_restart');
+const quitGame = document.querySelector('.btn_quit');
+const winnerWondow = document.querySelector(".winner");
+const winnerText = document.querySelector("winnerh2");
+
